037c1c93dcc4074fb5ed08325f475c0a61a696a9
JayShepherdDhani <jay.shepherd@dhani.com>
2021-10-21 10:34:18 +0100

    initial setup with database, makefile, database engine

---
+++ api/core/settings.py
@@ -0,0 +1,24 @@
+import os
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+env = Path('.') / '.env'
+load_dotenv(dotenv_path=env)
+
+
+class Settings:
+    # Project meta
+    TITLE: str = "4sale"
+    VERSION: str = "0.1.1"
+
+    # Database
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+settings = Settings()
